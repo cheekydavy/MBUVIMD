d6f4cd5a115a72046b93bbbc64e66a8f25be8908
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2025-05-07 14:17:03 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-     echo "Running load-npmrc.js to set up that .npmrc file..."
+     echo "Running load-npmrc.js to set up that fucking .npmrc file..."
      node mbuvi.js
-     echo "Installing dependencies like a badass..."
-     npm install --production
-     echo "Build done!"
+     if [ $? -ne 0 ]; then
+       echo "load-npmrc.js fucked up hard, bailing out..."
+       exit 1
+     fi
+     echo "Installing dependencies like a goddamn beast..."
+     npm install --production --loglevel=error
+     if [ $? -ne 0 ]; then
+       echo "npm install shit the bed, we’re done here..."
+       exit 1
+     fi
+     echo "Build crap finished, let’s roll!"
